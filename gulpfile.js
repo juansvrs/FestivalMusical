d1a8ca240f762e71b77e4ec849f82e56d49d9b36
juansvrs <juansvrs0321@gmail.com>
2022-09-06 20:18:52 -0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const{src,dest,watch} = require("gulp");
+const sass= require("gulp-sass")(require("sass"));
+const plumber = require("gulp-plumber");
+
+function css(done){
+    src('src/scss/**/*.scss').
+        pipe(plumber()).
+            pipe(sass()).
+                 pipe(dest("build/css"));
+    done();
+}
+function dev(done){
+    watch('src/scss/**/*.scss',css)
+    done();
+}
+function tarea(done){
+    console.log("Mi primer tarea aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+    done();
+}
+
+
+exports.tarea=tarea;
+exports.css=css;
+exports.dev=dev;
