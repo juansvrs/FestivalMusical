27fcfe0d05dc1ed536d1d3e5c63ca85ead2c857f
juansvrs <juansvrs0321@gmail.com>
2022-09-07 13:53:29 -0500

    comentario pre merge

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const{src,dest,watch} = require("gulp");
 const sass= require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
-//Hola
+//Hola te felicito que  bien actuas!
 function css(done){
     src('src/scss/**/*.scss').
         pipe(plumber()).
